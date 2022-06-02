7f7c32d37b939cd2f3f0659291222463552f747b
ashu78890 <ashish.choudhary@biztecno.net>
2022-06-02 07:58:54 +0530

    task done

--- src/theme.js
+++ src/theme.js
@@ -22,5 +22,15 @@ export const theme ={
         primary:'red',
         secondary:'blue',
         defaultL:'green'
+    },
+    bordersize:{
+        xs:'0.4rem',
+        sm:'0.3rem',
+        md:'0.5rem',
+        lg:'1rem',
+        exlg:'1.25rem',
+        ex2lg:'1.5rem',
+        ex3lg:'1.75rem',
+        ex4lg:'2rem'
     }
 }
